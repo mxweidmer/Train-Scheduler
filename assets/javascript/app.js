42125081e660c2f3b2d1cb711b582af7b62a826a
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-08 20:13:55 -0800

    added and initialized firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+var config = {
+    apiKey: "AIzaSyCiPeY5RT-XB40Dl8S2rjytmhdCCzdhcbI",
+    authDomain: "train-scheduler-df65b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-df65b.firebaseio.com",
+    projectId: "train-scheduler-df65b",
+    storageBucket: "train-scheduler-df65b.appspot.com",
+    messagingSenderId: "659352709648"
+};
+
+firebase.initializeApp(config);

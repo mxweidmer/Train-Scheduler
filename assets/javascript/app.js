135bda1b78ee2005e0a1bfd7e9d86c5f7671913b
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-08 21:02:16 -0800

    created button listener and validation for user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,3 +16,24 @@ var trainName = "";
 var destination = "";
 var firstTrain = "";
 var frequency = 0;
+
+$(".btn").on("click", function () {
+
+    if (($("#train-name").val().trim() !== "") &&
+        ($("#destination").val().trim() !== "") &&
+        ($("#frequency").val().trim() !== "") &&
+        (moment($("#train-time").val().trim(), "HH:mm").isValid() === true)
+    ) {
+        trainName = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        frequency = $("#frequency").val().trim();
+        firstTrain = $("#train-time").val().trim();
+
+        $("#train-name").val("");
+        $("#destination").val("");
+        $("#frequency").val("");
+        $("#train-time").val("");
+    } else {
+        alert("One of the forms was entered incorrectly");
+    }
+})
